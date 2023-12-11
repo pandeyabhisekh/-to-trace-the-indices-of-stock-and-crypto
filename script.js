db010f2c42dffdc7d06870c61d42e0cd16f2afdd
pandeyabhisekh <144727866+pandeyabhisekh@users.noreply.github.com>
2023-12-11 16:50:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-  fetchStockData();
-});
+function login() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var errorMessage = document.getElementById('error-message');
 
-function fetchStockData() {
-  // Using the Yahoo Finance API to fetch stock data
-  const apiUrl = 'https://query1.finance.yahoo.com/v7/finance/quote';
-  const symbols = ['AAPL', 'GOOGL', 'MSFT'];
-
-  const promises = symbols.map(symbol => {
-    return fetch(`${apiUrl}?symbol=${symbol}`)
-      .then(response => response.json())
-      .then(stockData => ({
-        symbol: symbol,
-        name: stockData.quoteType.longName,
-        price: stockData.quoteSummary.detail ? stockData.quoteSummary.detail.currentPrice.raw : null,
-      }))
-      .catch(error => {
-        console.error(`Error fetching stock data for ${symbol}:`, error);
-        return { symbol: symbol, name: 'N/A', price: 'N/A' };
-      });
-  });
-
-  Promise.all(promises)
-    .then(stockData => {
-      displayData('stockList', stockData);
-    })
-    .catch(error => console.error('Error fetching stock data:', error));
-}
-
-// The displayData function remains unchanged
-function displayData(listId, data) {
-  const listElement = document.getElementById(listId);
-  listElement.innerHTML = '';
-
-  data.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<strong>${item.symbol}</strong>: ${item.name} - $${item.price || 'N/A'}`;
-    listElement.appendChild(listItem);
-  });
+  // Basic validation (you can replace this with more secure authentication)
+  if (username === 'user' && password === 'pass') {
+    errorMessage.textContent = '';
+    alert('Login successful!');
+  } else {
+    errorMessage.textContent = 'Invalid username or password';
+  }
 }
